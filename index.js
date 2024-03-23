94b5f7470b4a598915d189ac3beb72478c4d8465
anurag <anuragbadwahe@gmail.com>
2024-03-23 23:41:02 +0530

    Created Routes to get the analytics of the app

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require("path");
 const urlRoute = require("./routes/url");
 const { connectToMongoDB } = require("./dbConfig");
 const URL = require("./models/url");
 const PORT = 8000;
 
 const app = express();
+
+//Connecting to views
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -16,7 +21,7 @@ connectToMongoDB("mongodb://localhost:27017").then(
   console.log("MongoDB Connected")
 );
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortURLId;
   const longURL = await URL.findOneAndUpdate(
     {
@@ -32,6 +37,25 @@ app.get("/:shortId", async (req, res) => {
   // Redirect to the longURL's redirectURL
   res.redirect(longURL.redirectURL);
 });
+
+app.get("/test", async (req, res) => {
+  const allUrl = await URL.find({});
+  return res.end(`
+  <html>
+  <head></head>
+  <body>
+    <ol>
+      ${allUrl.map((url) => {
+        return `<li>
+          ${url.shortURLId} - ${url.redirectURL} - ${url.totalVisits}
+        </li>`;
+      }).join('')}
+    </ol>
+  </body>
+  </html>
+  `);
+});
+
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:", PORT);
