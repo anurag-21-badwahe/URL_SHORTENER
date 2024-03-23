84ba5c40898f41ac751c8d07644c036254e4e975
anurag <anuragbadwahe@gmail.com>
2024-03-24 00:30:35 +0530

    Setup EJS and test the short id

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const path = require("path");
 const urlRoute = require("./routes/url");
+const staticRouter = require("./routes/staticRouter");
 const { connectToMongoDB } = require("./dbConfig");
 const URL = require("./models/url");
 const PORT = 8000;
 
 const app = express();
 
+connectToMongoDB("mongodb://localhost:27017").then(
+  console.log("MongoDB Connected")
+);
 //Connecting to views
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 
 app.use("/url", urlRoute);
+app.use("/", staticRouter);
 
 //Connection to mongodb
-connectToMongoDB("mongodb://localhost:27017").then(
-  console.log("MongoDB Connected")
-);
 
 app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortURLId;
@@ -37,25 +40,6 @@ app.get("/url/:shortId", async (req, res) => {
   // Redirect to the longURL's redirectURL
   res.redirect(longURL.redirectURL);
 });
-
-app.get("/test", async (req, res) => {
-  const allUrl = await URL.find({});
-  return res.end(`
-  <html>
-  <head></head>
-  <body>
-    <ol>
-      ${allUrl.map((url) => {
-        return `<li>
-          ${url.shortURLId} - ${url.redirectURL} - ${url.totalVisits}
-        </li>`;
-      }).join('')}
-    </ol>
-  </body>
-  </html>
-  `);
-});
-
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:", PORT);
